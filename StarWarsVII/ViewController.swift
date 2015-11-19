e1b7e9fc622737e821f3580184ac858ae4dc916e
José Manuel Rodríguez Moreno <idenjoe@idenjoe.es>
2015-11-19 20:40:25 +0100

    Initial Commit

---
+++ StarWarsVII/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StarWarsVII
+//
+//  Created by José Manuel Rodríguez Moreno on 19/11/15.
+//  Copyright © 2015 José Manuel Rodríguez Moreno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
