957fe1bcaaba3839f6c333795be034b82e5c24ce
José Manuel Rodríguez Moreno <idenjoe@idenjoe.es>
2015-11-20 00:00:30 +0100

    Añado ForceSensitive

--- StarWarsVII/Models/ForceSensitive.swift
+++ StarWarsVII/Models/ForceSensitive.swift
@@ -6,5 +6,70 @@
 //  Copyright © 2015 José Manuel Rodríguez Moreno. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
+
+class ForceSensitive: StarWarsCharacter {
+    
+    //MARK: - Properties
+    
+    let midichlorians : Int
+    
+    //MARK: - Initialization
+    init(firstName: String?,
+        lastName: String?,
+        alias: String?,
+        soundData: NSData,
+        photo: UIImage,
+        url: NSURL,
+        affiliation: StarWarsAffiliation,
+        midichlorians: Int) {
+        
+            self.midichlorians = midichlorians
+            super.init(firstName: firstName,
+                lastName: lastName,
+                alias: alias,
+                soundData: soundData,
+                photo: photo,
+                url: url,
+                affiliation: affiliation)
+    }
+    
+    //MARK: - Convenience
+    
+    convenience init(jediWithFirstName: String?,
+        lastName: String?,
+        alias: String?,
+        soundData: NSData,
+        photo: UIImage,
+        url: NSURL,
+        midichlorians: Int){
+            
+            self.init(firstName: jediWithFirstName,
+                lastName: lastName,
+                alias: alias,
+                soundData: soundData,
+                photo: photo,
+                url: url,
+                affiliation: .RebelAlliance,
+                midichlorians: midichlorians)
+    }
+    
+    convenience init(sithWithFirstName: String?,
+        lastName: String?,
+        alias: String?,
+        soundData: NSData,
+        photo: UIImage,
+        url: NSURL,
+        midichlorians: Int){
+            
+            self.init(firstName: sithWithFirstName,
+                lastName: lastName,
+                alias: alias,
+                soundData: soundData,
+                photo: photo,
+                url: url,
+                affiliation: .GalacticEmpire,
+                midichlorians: midichlorians)
+    }
+}
