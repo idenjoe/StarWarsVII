a8a92662fe813b8dc48436df4fbd0cee25156693
José Manuel Rodríguez Moreno <idenjoe@idenjoe.es>
2015-11-19 21:24:44 +0100

    Clase del modelo monda y lironda.

---
+++ StarWarsVII/Models/StarWarsCharacter.swift
@@ -0,0 +1,22 @@
+//
+//  StarWarsCharacter.swift
+//  StarWarsVII
+//
+//  Created by José Manuel Rodríguez Moreno on 19/11/15.
+//  Copyright © 2015 José Manuel Rodríguez Moreno. All rights reserved.
+//
+
+import UIKit
+
+class StarWarsCharacter {
+    
+    //MARK: - Properties    
+    let firstName   :   String = ""
+    let lastNamame  :   String = ""
+    let alias       :   String = ""
+//    let photo       :   UIImage
+//    let url         :   NSURL
+    let affiliation :   String = ""
+    
+    
+}
