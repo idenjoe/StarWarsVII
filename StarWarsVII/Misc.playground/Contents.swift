c29f999c9eae81b1ba116bfd300529fe1eb7e705
José Manuel Rodríguez Moreno <idenjoe@idenjoe.es>
2015-12-11 20:13:45 +0100

    Añadida toda la funcionalidad a StarWarsUniverse

--- StarWarsVII/Misc.playground/Contents.swift
+++ StarWarsVII/Misc.playground/Contents.swift
@@ -68,3 +68,81 @@ class Numbers{
         }
     }
 }
+
+
+//: Genéricos
+
+func swap(inout a:Int, inout b:Int){
+    let aux = a
+    a = b
+    b = aux
+}
+
+var n = 2
+var m = 3
+
+(n,m)
+swap(&n, b: &m)
+(n,m)
+// Función genérica o de plantilla
+func swap<T>(inout a:T, inout b:T){
+    let aux = a
+    a = b
+    b = aux
+}
+
+var x = 2.0
+var y = 3.0
+
+(x,y)
+swap(&x, b: &y)
+(x,y)
+
+var p = NSURL(string: "http://www.google.es")
+var q = NSURL(string: "http://www.apple.es")
+
+(p,q)
+swap(&p, b: &q)
+(p,q)
+
+
+// Clase genérica
+
+struct Pair <T,U>{
+    let a : T
+    let b : U
+}
+
+let r = Pair(a: 3, b: 6)
+Pair(a: "hola", b: NSDate())
+
+// Clase genérica
+class Trio<T, U, V>{
+    let first : T
+    let middle : U
+    let last : V
+    
+    init(first: T,middle: U, last: V){
+        self.first = first
+        self.middle = middle
+        self.last = last
+    }
+}
+
+let g = Trio(first: 5, middle: NSDate(), last: NSURL(string: "http://www.apple.es"))
+print(g.first)
+print(g.middle)
+print(g.last)
+
+// Parejas tiquismiquis
+// restricciones a los tipos genéricos
+
+struct ComparablePair<T:Comparable, U:Comparable> {
+    let w : T
+    let x : U
+}
+
+ComparablePair(w: 3, x: "holi")
+
+
+let list : Array<Int> = [1,2,3]
--- StarWarsVII/StarWarsUniverse.swift
+++ StarWarsVII/StarWarsUniverse.swift
@@ -9,11 +9,84 @@
 import UIKit
 
 // Carta a los Reyes Magos:
-// Total de personajes
-// Total de las afiliaciones
-// El nombre de una afiliación
-// El personaje n de la afiliación m
 
 class StarWarsUniverse {
+    
+    //MARK: - Private
+    // Guardo los personajes en un diccionario cuyas claves son las afiliaciones
+    // y los valores son arrays de StarWarsCharacters
+    private var characters : [StarWarsAffiliation:[StarWarsCharacter]]
+    
+    //MARK: - Initialization
+    init(arrayOfCharacters : [StarWarsCharacter])
+    {
+        // Inicializamos el diccionario vacío
+        characters = Dictionary<StarWarsAffiliation,Array<StarWarsCharacter>>()
+        for each in StarWarsAffiliation.cases() {
+            characters[each] = Array<StarWarsCharacter>()
+        }
+        // Patearse los personajes y según su afiliación los metemos
+        // en un sitio y otro
+        for character in arrayOfCharacters{
+            // Según la afiliación lo metemos en un sitio u otro
+            let aff = character.affiliation
+            
+            characters[aff]?.append(character)
+        }
+    }
+    
+    //MARK: - Public
+    var countCharacters : Int{
+        
+        // Total de personajes
+        get{
+            // Patearse el diccionario e ir sumando la cantidad de personajes de cada afiliación
+            var total = 0
+            for affiliation in StarWarsAffiliation.cases(){
+                total += countCharacters(affiliation)
+            }
+            
+            return total
+        }
+    }
+    
+    var countAffiliations: Int{
+        // Total de las afiliaciones
+        get{
+            return StarWarsAffiliation.countAffiliations()
+        }
+    }
+    
+    // Total de personajes para cada afiliación
+    func countCharacters(inAffiliation: StarWarsAffiliation) -> Int{
+        // Obtengo el array de la afiliación y le pregunto cual es su tamaño
+        if let chars = characters[inAffiliation] {
+            return chars.count
+        }
+        
+        return 0
+    }
+    
+    // El nombre de una afiliación
+    func affiliationName(atIndex: Int) -> String{
+        // todo
+        let affiliations = Array(characters.keys)
+        let aff = affiliations[atIndex]
+        return StarWarsAffiliation.nameOfAffiliation(aff)
+    }
+    
+    // El personaje n de la afiliación m
+    subscript(idx: Int, inAffiliation aff: StarWarsAffiliation) -> StarWarsCharacter?{
+        get{
+            // Obtener el array de la afiliación
+            // el personaje en la posición
+            // Deberes
+            if let chars = characters[aff]{
+                return chars[idx]
+            }
+            
+            return nil
+        }
+    }
     
 }
